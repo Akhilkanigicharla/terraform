20568cf97720362ea3f2ecf842597f5f504a9abf
Akhilkanigicharla <kanigicharlaakhil9@gmail.com>
2021-06-18 20:23:24 +0530

    hi

--- outputs/sample.tf
+++ outputs/sample.tf
@@ -1,7 +1,13 @@
 output "sample" {
-  value = "hellow world"
+ // value = "hellow world"
+  value = var.sample
 }
 
 output "sample1" {
   value = "hello world 1"
+}
+
+
+variable "sample" {
+  default = "hello hard"
 }
